788b290b2de819ae398188b96e892b572f3f6eec
corujiinhagg <luiz.paulo21@outlook.com>
2025-11-13 17:03:59 +0000

    Entregando nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 
-//
+void moverTorre(int casas){
+    if(casas > 0){
+        printf("Torre Direita\n");
+        moverTorre(casas - 1);
+    }
+}
+
+void moverBispo(int casas){
+    if(casas > 0){
+        printf("Bispo Cima - Direita\n");
+        moverBispo(casas - 1);
+    }
+}
+
+void moverRainha(int casas){
+    if(casas > 0){
+        printf("Rainha Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
 
 int main() {
 
-    // Movendo a torre 5 casas para direita 
+    // Movendo Torre, Bispo e Rainha com Recursividade
+    moverTorre(5);
+    moverBispo(5);
+    moverRainha(8);
+
+    /* Movendo a torre 5 casas para direita com loop
     for ( int t = 0 ; t < 5 ; t++ ){
         printf("Torre Direita \n");
-    }
-
+    }*/
     
-    // Movendo Bispo 5 casas diagonal para cima - direita
+    /* Movendo Bispo 5 casas diagonal para cima - direita com loop
 
     int b = 0;
 
@@ -18,9 +41,9 @@ int main() {
         
         printf("Bispo Cima - Direita \n");
         b++;
-    }
-    
-    // Movendo Rainha 8 casas para esquerda
+    } */
+
+    /* Movendo Rainha 8 casas para esquerda com Loop
 
     int r = 0;
 
@@ -28,18 +51,19 @@ int main() {
         printf("Rainha Esquerda \n");
         r++;
 
-    } while ( r < 8 );
+    } while ( r < 8 ); */
 
-    // Movendo a peça Cavalo 2 casa para baixo e 1 casa para esquerda
+
+    // Movendo a peça Cavalo 2 casa para baixo e 1 casa para esquerda com Loop
 
     int movimentoCavalo = 1;
 
     while (movimentoCavalo--){
 
         for(int c = 0; c < 2; c++){
-            printf("Cavalo Baixo \n");
+            printf("Cavalo Cima\n");
         }
-        printf("Cavalo Esquerda \n");
+        printf("Cavalo Direita\n");
     }
 
     printf("Rodada encerrada, aguardando próximo jogador...\n");
